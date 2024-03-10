5ee7b7e05c7230dc0e9595f35fa42609b5408513
Gustavo Cardoso <self.gustavocorrea@gmail.com>
2024-03-09 22:36:52 -0300

    wip

--- build.sh
+++ build.sh
@@ -1,20 +1,17 @@
-#!/bin/sh
+#!/bin/sh -e
 
-versions=("3.9" "3.10" "3.11" "3.12")
-compat=("musllinux_1_1" "manylinux_2_17" "manylinux_2_28" "manylinux_2_34")
-target="x86_64-unknown-linux-gnu"
+versions=('3.9' '3.10' '3.11' '3.12')
+compat=('musllinux_1_2' 'manylinux_2_17' 'manylinux_2_28' 'manylinux_2_34')
+target='x86_64-unknown-linux-gnu'
 
-for version in $versions; do
-    for build in $compat; do
+for version in "${versions[@]}"; do
+    for build in "${compat[@]}"; do
+        echo "${version} - ${build};"
         maturin build \
             --release \
-            --target "$target" \
+            --target "${target}" \
             --interpreter $(which "python${version}") \
-            --compatibility "$build" \
+            --compatibility "${build}" \
             --zig
     done
 done
-
-# for version in "3.9" "3.10" "3.11"; do
-#     maturin build --release --target --zig --interpreter $(which "python${version}")
-# done
